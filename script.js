6b5cdf82d7a0de8a470a7147e5619597a8e59a26
Rithigaa <108213520+Rithigaa@users.noreply.github.com>
2024-01-06 15:08:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,54 @@
-class TodoList {
-  #form = document.getElementById('form');
-  #input = document.getElementById('input');
-  #todosUL = document.getElementById('todos');
-  #todos = [];
+const inputtdl = document.querySelector('.textarea')
+const buttontdl = document.querySelector('.buttoninput')
+const listtdl = document.querySelector('.todolist')
 
-  constructor() {
-    this.loadTodos();
-    this.bindEvents();
-  }
-
-  loadTodos() {
-    const storedTodos = JSON.parse(localStorage.getItem('todos'));
-    if (storedTodos) {
-      this.#todos = storedTodos;
-      this.renderTodos();
-    }
-  }
-
-  bindEvents() {
-    this.#form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      this.addTodo();
-    });
-  }
-
-  addTodo(todoText = this.#input.value) {
-    if (todoText) {
-      const todo = { text: todoText, completed: false };
-      this.#todos.push(todo);
-      this.renderTodo(todo);
-      this.#input.value = '';
-      this.saveTodos();
-    }
-  }
-
-  renderTodo(todo) {
-    const todoEl = document.createElement('li');
-    if (todo.completed) {
-      todoEl.classList.add('completed');
-    }
-    todoEl.innerText = todo.text;
-
-    todoEl.addEventListener('click', () => {
-      todo.completed = !todo.completed;
-      todoEl.classList.toggle('completed');
-      this.saveTodos();
-    });
-
-    todoEl.addEventListener('contextmenu', (e) => {
-      e.preventDefault();
-      this.removeTodo(todoEl);
-    });
-
-    this.#todosUL.appendChild(todoEl);
-  }
-
-  renderTodos() {
-    this.#todosUL.innerHTML = ''; // Clear existing todos
-    this.#todos.forEach(this.renderTodo);
-  }
-
-  removeTodo(todoEl) {
-    const todoIndex = this.#todos.findIndex(todo => todo.text === todoEl.innerText);
-    this.#todos.splice(todoIndex, 1);
-    todoEl.remove();
-    this.saveTodos();
-  }
-
-  saveTodos() {
-    localStorage.setItem('todos', JSON.stringify(this.#todos));
-  }
+function clickButton(e) {
+    e.preventDefault()
+    addTodo()
 }
 
-new TodoList();
+// adding todoList
+function addTodo() {
+    const itemall = document.createElement('div')
+    itemall.classList.add('itemall')
+
+    const item = document.createElement('p')
+    item.classList.add('item')
+    item.innerText = inputtdl.value
+    itemall.appendChild(item)
+
+    if (inputtdl.value === '') return
+
+    const checkbutton = document.createElement("button")
+    checkbutton.innerHTML = '<i class="fa-solid fa-check"></i>'
+    checkbutton.classList.add("check-button")
+    itemall.appendChild(checkbutton)
+
+    const trashbutton = document.createElement("button")
+    trashbutton.innerHTML = '<i class="fa-solid fa-trash"></i>'
+    trashbutton.classList.add("trash-button")
+    itemall.appendChild(trashbutton)
+
+    listtdl.appendChild(itemall)
+    inputtdl.value = ''
+}
+
+// checking and delete todoList 
+function okdel(e) {
+    const item = e.target
+
+    // check
+    if (item.classList[0] === 'check-button') {
+        const todolist = item.parentElement
+        todolist.classList.toggle('checklist')
+    }
+
+    // delete
+    if (item.classList[0] === 'trash-button') {
+        const todolist = item.parentElement
+        todolist.remove()
+    }
+}
+
+buttontdl.addEventListener('click', clickButton)
+listtdl.addEventListener('click', okdel)
